89b7ad05f539e1b6cbeb0336bcfc79b2a418ef90
Wiput Pootong <wiput.pootong@gmail.com>
2020-04-27 00:25:57 +0700

    :sparkles: working

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-assert str("a") == "a"
+class Student:
+    def __init__(self):
+        self.name = "Hello"
+
+    def __str__(self):
+        return self.name
+
+
+assert str(Student()) == "Hello"
