33a686d7edfa406cfc621ae2a1f3d46a38064fa5
Wiput Pootong <wiput.pootong@gmail.com>
2020-04-27 00:28:00 +0700

    :sparkles: working

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 class Student:
-    def __init__(self):
-        self.name = "Hello"
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
 
     def __str__(self):
         return self.name
 
 
-assert str(Student()) == "Hello"
+assert str(Student("John", 80)) == "John"
