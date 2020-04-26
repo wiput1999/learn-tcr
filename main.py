cea2fe1656c1b263d3809eabbd8cb0f6b37c4956
Wiput Pootong <wiput.pootong@gmail.com>
2020-04-27 00:28:45 +0700

    :sparkles: working

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ class Student:
     def __str__(self):
         return self.name
 
+    def get_grade(self):
+        if self.score >= 80:
+            return "A"
+        return "F"
+
 
 assert str(Student("John", 80)) == "John"
